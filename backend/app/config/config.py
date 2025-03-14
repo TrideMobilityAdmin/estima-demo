86c6d39673dda909e91e8763621c954440d03651
radhika <radhika@gmail.com>
2025-03-14 16:06:48 +0530

    changes in config

--- backend/app/config/config.py
+++ backend/app/config/config.py
@@ -1,8 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str ="mongodb://admin:admin123@10.100.3.13:27017/"  
-    DB_NAME: str = "gmr-mro-staging-5y"
+    DATABASE_URL: str ="mongodb://admin:Tride%401234@telematics-mongo1.evrides.in:22022,telematics-mongo2.evrides.in:22022,telematics-mongo3.evrides.in:22022/?authSource=admin&replicaSet=trideRepl"  
+    DB_NAME: str = "gmr-mro"
     SECRET_KEY: str = "AIzaSyBtLc5L8WECfgz6i1NzzNU7uFfhIig7DyQ"
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 24*60
